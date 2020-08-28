fbfa2ac195dcd8af36f1e471a319707f02d2c2f9
edwardweijieai <edwardweijieai@gmail.com>
2020-08-28 07:05:15 +0000

    [fixed] pretty url and using promethues library

---
+++ controllers/monitor/PrometheusController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace app\controllers\monitor;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class PrometheusController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * show metrics.
+     * 
+     * example return backyard_websites_number_sum 
+     *
+     * @return string
+     */
+    public function actionMetrics()
+    {
+        // \Prometheus\CollectorRegistry::getDefault()->getOrRegisterCounter('', 'some_quick_counter', 'just a quick measurement')->inc();
+        echo 'backyard_websites_number_sum 30';
+    }
+}
